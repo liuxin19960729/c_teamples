43d9271659ac33974a59ecf03ffac51efde4aadb
liuixn <963875368@qq.com>
2022-10-19 16:36:52 +0800

    cpp 学习 添加第五章

--- codes/chapter4/4_2_1_t.cpp
+++ codes/chapter4/4_2_1_t.cpp
@@ -2,8 +2,28 @@
 
 using namespace std;
 
+template <typename T, int Val>
+T addV(T const &x)
+{
+    return x + Val;
+}
+
+void zz(int a, int (*fun)(int const &))
+{
+    auto c = fun(a);
+
+    cout << c << endl;
+}
+
+template <typename T, char * pch>
+T addVPch(T const &x)
+{
+    return x + Val;
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    // zz(10,addV<int,300>);
+    
     return 0;
 }
